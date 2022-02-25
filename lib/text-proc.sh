e8178ca1f83e607aa988668b6c2e0d5168d33bee
Sandor Semsey <sandor@es-progress.hu>
2022-02-25 16:32:14 +0100

    urlencode/decode

--- lib/text-proc.sh
+++ lib/text-proc.sh
@@ -45,3 +45,40 @@ read-file-cfg(){
     # Delete section headers
     sed -r -e '/^\s*\[/ d' <<<"${contents}"
 }
+
+## URL-encode string
+##
+## @param   $1  String to encode
+################################
+urlencode() {
+    local string="${1:?"String missing"}"
+    local encoded=""
+    local pos char enc_char
+
+    # Encode each individual chars sequentially
+    for (( pos=0 ; pos<${#string} ; pos++ )); do
+        char="${string:$pos:1}"
+        case "${char}" in
+            [-_.~a-zA-Z0-9]) enc_char="${char}" ;;
+            *) printf -v enc_char '%%%02x' "'${char}" ;;
+        esac
+        encoded+="${enc_char}"
+    done
+
+    printf "\n%s\n" "${encoded}"
+}
+
+## Decode URL-encoded string
+##
+## @param   $1  String to decode
+################################
+urldecode(){
+    local encoded="${1:?"URL-encoded string missing"}"
+
+    # Change + to space
+    encoded="${encoded//+/ }"
+    # Change % to \x
+    encoded="${encoded//%/\\x}"
+    # Print escaped chars e.g. \x3A => :
+    printf "\n'%b'\n" "${encoded}"
+}
